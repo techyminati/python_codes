931a026ff5100c1ac65084039ae54c40a8ee3e83
techyminati <sinha.aryan03@gmail.com>
2021-10-01 16:35:17 +0530

    codes: Add Lineage Sync Script

---
+++ los.py
@@ -0,0 +1,18 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+# (c) TechyMinati ( Aryan Sinha ) <sinha.aryan03@gmail.com>
+import os
+import sys
+print ("==========================================")
+print ("      Welcome to LineageOS Sync Script    ")
+print ("==========================================")
+n=input("Press Y to Continue, N to Exit: ")
+if (n=="Y") or (n=="y") :
+    print("Starting Sync..")
+    print (" Warning: Repo is needed to be installed in prior of running this script)
+    os.system('repo init -u git://github.com/LineageOS/android.git -b lineage-18.1')
+    print ("Starting Repo Sync")
+    os.system('repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)')
+else :
+    print("Re run script to start sync incase you mistakenly pressed 'N' ")
+     
