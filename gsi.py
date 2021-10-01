4507101e5b597dd4db0e922a9744abded1d950e4
Gian Paolo Estacio <67692232+eun0115@users.noreply.github.com>
2021-10-01 19:33:05 +0800

    gsi.py: Update gsi.py

--- gsi.py
+++ gsi.py
@@ -6,20 +6,34 @@ import sys
 print ("==========================================")
 print ("      Welcome to sGSI Building Script    ")
 print ("==========================================")
-n=input("Press Y to Continue, N to Exit: ")
+n=input("Press Y to Proceed, N to Abort: ")
 if (n=="Y") or (n=="y") :
     print("Cloning ErfanGSIs Sourcecode")
     print (" Warning: this is a Fork of ErfanGSIs, Adapted for our Usage")
     os.system('git clone --recurse-submodules https://github.com/sinhaaryan03/ErfanGSIs')
     os.system('sudo chmod -R 777 ErfanGSIs')
+    import time
     print ("Initialising enviroment for GSI Building")
+    time.sleep(5)
+    print ("Enviroment Initialised")
     os.system('cd ErfanGSIs && sudo bash setup.sh')
     print("==========================================")
-    print(" Ready to Build GSI")
+    print(" Are you ready to Build GSI?")
     print("==========================================")
     os.system('sudo bash assets/gsi.sh')
+    n=input("Press Y to Proceed, N to Abort: ")
+if (n=="Y") or (n=="y") :
+    import time
+    print("==========================================")
+    print(" Initialising to Build GSI")
+    print("==========================================")
+    time.sleep(5)
     print("==========================================")
     print(" GSI Build Successfully, find it On ErfanGSIs/output ")
     print("==========================================")
 else:
-    print(" Thanks , If you want to restart GSI Building, re-run this script")
+    import time
+    time.sleep(5)
+    print("Thanks for using the Tool")
+    print("If you want to restart GSI Building")
+    print("re-run this script")
