1e0220e039f8f5219888ff2d87545d3969c6a0c3
Gian Paolo Estacio <67692232+eun0115@users.noreply.github.com>
2021-10-01 22:55:18 +0800

    codes: Add PBRP Sync Script

---
+++ pbrp.py
@@ -0,0 +1,34 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+# (c) eun0115 ( eun ) <gianpaoloestacio5@gmail.com>
+import os
+import sys
+os.makedirs('pbrp')
+os.system('sudo apt update')
+os.system('sudo apt upgrade')
+os.system('git clone https://github.com/akhilnarang/scripts')
+os.chdir('scripts')
+os.system('bash setup/android_build_env.sh')
+os.chdir('../pbrp')
+import time
+print ("==========================================================")
+print ("      Welcome to PitchBlackRecoveryProject Sync Script    ")
+print ("==========================================================")
+time.sleep(3)
+import time
+print ("==========================================================")
+print ("              Do you want to sync the Source?             ")
+print ("==========================================================")
+time.sleep(1)
+n=input("Press Y to Continue, N to Exit: ")
+if (n=="Y") or (n=="y") :
+    print("Starting Sync..")
+    print(" Warning: Repo is needed to be installed in prior of running this script")
+    os.system('repo init -u git://github.com/PitchBlackRecoveryProject/manifest_pb -b android-11.0')
+    print ("Starting Repo Sync")
+    os.system('repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)')
+else :
+    import time
+    time.sleep(3)
+    print("Re-run the script again to start sync incase you mistakenly pressed 'N' ")
+     
