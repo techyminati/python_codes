e58651dedcaa695f191e394fe5155946c7b9e685
Sarthak Roy <sarthakroy2002@gmail.com>
2021-10-02 03:09:06 +0530

    Add a simple Addition python program

---
+++ add.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+# (c) Sarthak Roy (sarthakroy2002) <sarthakroy2002@gmail.com>
+#
+# Take Input of two numbers from the user
+n1 = input('Enter first number: ')
+n2 = input('Enter second number: ')
+
+# Add the two numbers taken from the user
+sum = float(n1) + float(n2)
+
+# Display the sum of two numbers
+print('The sum of is {0}'.format(sum))
