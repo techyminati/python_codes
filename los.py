42adc308dcaa815d56e1c782a5de0f5e41f28cd0
Sushrut1101 <guptasushrut@gmail.com>
2022-10-02 11:52:08 +0530

    los: Fix Syntax

--- los.py
+++ los.py
@@ -9,7 +9,7 @@ print ("==========================================")
 n=input("Press Y to Continue, N to Exit: ")
 if (n=="Y") or (n=="y") :
     print("Starting Sync..")
-    print (" Warning: Repo is needed to be installed in prior of running this script)
+    print (" Warning: Repo is needed to be installed in prior of running this script")
     os.system('repo init -u git://github.com/LineageOS/android.git -b lineage-18.1')
     print ("Starting Repo Sync")
     os.system('repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)')
