0b4db6c6a8488b64b1cb259d19a8a8d9078a9676
Seven7teen <68948140+Seven7teen@users.noreply.github.com>
2022-10-03 16:13:03 +0530

    Create print-odd-numbers-in-a-list.py

---
+++ print-odd-numbers-in-a-list.py
@@ -0,0 +1,11 @@
+# Python program to print odd Numbers in a List
+
+# list of numbers
+list1 = [10, 21, 4, 45, 66, 93]
+
+# iterating each number in list
+for num in list1:
+
+	# checking condition
+	if num % 2 != 0:
+	print(num, end=" ")
