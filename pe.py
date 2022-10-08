b815ee8fe1336dab56dc8ca32d2f6174ececd4ad
Deep Roy <sarthakroy2002@outlook.com>
2022-10-08 18:36:53 +0530

    PE: Ask for Branch to sync

--- pe.py
+++ pe.py
@@ -7,7 +7,8 @@ n=input("Press Y to Continue, N to Exit: ")
 if (n=="Y") or (n=="y") :
     print("Starting Sync..")
     print (" Warning: Repo is needed to be installed in prior of running this script)
-    os.system('repo init -u https://github.com/PixelExperience/manifest -b eleven')
+    branch = input("Enter the branch name to sync: ")
+    os.system(f'repo init -u https://github.com/PixelExperience/manifest -b {branch}')
     print ("Starting Repo Sync")
     os.system('repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)')
 else :
