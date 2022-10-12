1ba2df74572e202baef7ff5d74c6b1496ed02a5d
suksome <sakshamsai26sep@gmail.com>
2022-10-12 21:43:50 +0530

    Modified add.py

--- add.py
+++ add.py
@@ -1,13 +1,2 @@
-#!/usr/bin/env python3
-# SPDX-License-Identifier: GPL-3.0-or-later
-# (c) Sarthak Roy (sarthakroy2002) <sarthakroy2002@gmail.com>
-#
-# Take Input of two numbers from the user
-n1 = input('Enter first number: ')
-n2 = input('Enter second number: ')
-
-# Add the two numbers taken from the user
-sum = float(n1) + float(n2)
-
-# Display the sum of two numbers
-print('The sum of is {0}'.format(sum))
+sum = int(input("Enter first number: ")) + int(input("Enter second number: "))
+print("The sum of is",sum)
