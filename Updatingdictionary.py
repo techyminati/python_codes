641c8f58dff0adbd5e6af6b1bec30a9e238fc416
budhdev04 <115720020+budhdev04@users.noreply.github.com>
2022-10-14 20:57:31 +0530

    Create Updatingdictionary.py

---
+++ Updatingdictionary.py
@@ -0,0 +1,6 @@
+
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+dict['Age'] = 8; 
+dict['School'] = "DPS School";
+print "dict['Age']: ", dict['Age']
+print "dict['School']: ", dict['School']
