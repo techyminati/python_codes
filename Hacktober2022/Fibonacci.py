ebdfe4645e622af85ffdd310f2ed06a7aee91c9f
PMorey22 <prmorey11@gmail.com>
2023-10-05 15:12:03 +0530

    Update Fibonacci.py

--- Hacktober2022/Fibonacci.py
+++ Hacktober2022/Fibonacci.py
@@ -1,5 +1,16 @@
-# Program to display the Fibonacci sequence up to n-th term
-
+By: @agam778
+input_number = int(input("Enter the number of fibonacci numbers to be generated: "))
+first_number = 0
+second_number = 1
+print()
+print("Fibonacci series of", input_number, "numbers is:")
+print(first_number)
+print(second_number)
+for i in range(2, input_number):
+    next_number = first_number + second_number
+    print(next_number)
+    first_number = second_number
+    second_number = next_number
 nterms = int(input("How many terms? "))
 
 # first two terms
